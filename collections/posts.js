ee0b8a67b73901f63ca25ad3cf8d86cf199224d0
YSTYLE <lxy5266@live.com>
2015-03-18 04:16:01 +0800

    添加权限

--- collections/posts.js
+++ collections/posts.js
@@ -8,39 +8,47 @@ Posts = new Mongo.Collection('posts');
 Meteor.methods({
     postInsert: function (postAttributes) {
         // 校验器
-        check(Meteor.userId(),String);
-        check(postAttributes,{
-            title:String,
-            url:String
+        check(Meteor.userId(), String);
+        check(postAttributes, {
+            title: String,
+            url: String
         });
 
-        if(Meteor.isServer){
-            postAttributes.title += "(Service)";
-            // 等待5秒
-            Meteor._sleepForMs(5000);
-        }else{
-            postAttributes.title += "(client)";
-        }
-
-        var postWithSameList = Posts.findOne({url:postAttributes.url});
-        if(postWithSameList){
+        var postWithSameList = Posts.findOne({url: postAttributes.url});
+        if (postWithSameList) {
             return {
-                postExists:true,
-                _id:postWithSameList._id
+                postExists: true,
+                _id: postWithSameList._id
             }
         }
 
         var user = Meteor.user();
         // 设置默认值
-        var post = _.extend(postAttributes,{
-            userId:user._id,
-            author:user.username,
-            submitted:new Date(),
-            flagged:false
+        var post = _.extend(postAttributes, {
+            userId: user._id,
+            author: user.username,
+            submitted: new Date(),
+            flagged: false
         });
         var postId = Posts.insert(post);
         return {
-            _id:postId
+            _id: postId
         };
+    }
+});
+
+Posts.allow({
+    update: function (userId, post) {
+        return ownsDocument(userId, post);
+    },
+    remove: function (userId, post) {
+        return ownsDocument(userId, post);
+    }
+});
+
+Posts.deny({
+    update: function (userId, post, fieldNames) {
+        // 只能修改如下两个字段
+        return (_.without(fieldNames, 'url', 'title').length > 0);
     }
 });
--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@
  * Created by ystyle on 15-3-17.
  */
 Meteor.publish('posts', function () {
-    return Posts.find(/*{flagged:false},{fields:{
+    return Posts.find({flagged:false},{fields:{
         flagged:false
-    }}*/);
+    }});
 });
