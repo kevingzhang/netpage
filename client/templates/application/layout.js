d54e079ad4452bb05254f2e5eb326fd0c5ca9eb1
YSTYLE <lxy5266@live.com>
2015-03-18 22:41:34 +0800

    页面切换动画

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -7,3 +7,19 @@ Template.layout.helpers({
         return Session.get('pageTitle');
     }
 });
+
+Template.layout.rendered = function() {
+    this.find('#main')._uihooks = {
+        insertElement: function(node, next) {
+            $(node)
+                .hide()
+                .insertBefore(next)
+                .fadeIn();
+        },
+        removeElement: function(node) {
+            $(node).fadeOut(function() {
+                $(this).remove();
+            });
+        }
+    }
+}
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -10,6 +10,13 @@ Template.postsList.helpers({
 
 Template.postsList.rendered = function () {
     this.find('.wrapper')._uihooks = {
+        insertElement: function (node,next) {
+          $(node)
+              .hide()
+              .insertBefore(next)
+              .fadeIn();
+
+        },
         moveElement: function (node, next) {
             var $node = $(node),$next = $(next);
             var oldTop = $node.offset().top;
@@ -37,6 +44,11 @@ Template.postsList.rendered = function () {
                 $node.addClass('animate').css('top', 0);
                 $inBetween.addClass('animate').css('top', 0);
             }
+        },
+        removeElement: function (node) {
+            $(node).fadeOut(function () {
+                $(this).remove();
+            });
         }
     }
 };
